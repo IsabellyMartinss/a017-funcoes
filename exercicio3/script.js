105c0c9b41daafcb065db360bc40477f2ea16826
Isabelly Martins <isabellyksml@gmail.com>
2022-08-22 22:15:13 -0300

    Exercícios Concluídos - Aula 017 - Functions

---
+++ exercicio3/script.js
@@ -0,0 +1,20 @@
+let a = Number(prompt('Digite um número'))
+let b = Number(prompt('Digite mais um número'))
+
+function soma(a,b){
+    return a+b
+}
+
+function subtracao(a,b){
+    return a-b
+}
+
+function multiplicacao(a,b){
+    return a*b
+}
+
+function divisao(a,b){
+    return a/b
+}
+
+console.log(soma(a,b), subtracao(a,b), multiplicacao(a,b), divisao(a,b))
